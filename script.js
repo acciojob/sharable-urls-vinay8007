6c07766ad1238094a7f7bcf76c1b12a4695b851f
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2026-06-17 21:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// your code here
+const nameInput = document.getElementById("name");
+const yearInput = document.getElementById("year");
+const submitBtn = document.getElementById("button");
+const urlHeader = document.getElementById("url");
+
+submitBtn.addEventListener('click', function(event){
+  event.preventDefault();
+
+  let baseUrl = "https://localhost:8080/";
+
+  let nameValue = nameInput.value.trim();
+  let yearValue = yearInput.value.trim();
+  let queryParams = [];
+
+  if(nameValue !== ""){
+    queryParams.push(`name=${encodeURIComponent(nameValue)}`);
+  }
+  if(yearValue !== ""){
+    queryParams.push(`year=${encodeURIComponent(yearValue)}`);
+  }
+  if(queryParams.length > 0){
+    urlHeader.textContent = baseUrl + "?" + queryParams.join("&");
+  } else {
+    urlHeader.textContent = baseUrl;
+  }
+});
